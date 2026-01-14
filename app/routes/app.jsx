5705ae175cbf63297a84651d3b1e8d51d0ae3102
amith2083 <amith143.km143@gmail.com>
2026-01-15 01:53:14 +0530

    read.me

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -14,6 +14,7 @@ export const loader = async ({ request }) => {
 
 export default function App() {
   const { apiKey } = useLoaderData();
+ 
 
   return (
     <AppBridgeProvider embedded apiKey={apiKey}>
